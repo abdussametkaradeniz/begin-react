83b133eaba62baa0466a9fffa0c0aeac7f8b08aa
abdussamet karadeniz <abdussametgosukaradeniz@gmail.com>
2022-03-06 08:39:02 +0300

    tutorials değişiklikleri güncellendi

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Card from './component/Card';
+import Collapse from './component/Collapse';
 
+
+const App = () => {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='card-group w-100'>
+                    <div className='col'>
+                        <Collapse href="CollapseExample1">
+                            <Card
+                                cardTitle="Card Title I"
+                                cardText="lorem ıpsum dolor sit amet"
+                                updatedTime="3 min ago"
+                                image="https://picsum.photos/id/1/200/300"
+                            ></Card>
+                        </Collapse>
+                    </div>
+                    <div className='col'>
+                        <Collapse href="CollapseExample2">
+                            <Card
+                                cardTitle="Card Title II"
+                                cardText="lorem ıpsum dolor sit amet"
+                                updatedTime="3 min ago"
+                                image="https://picsum.photos/id/1/200/300"
+                            ></Card>
+                        </Collapse>
+                    </div>
+                    <div className='col'>
+                        <Collapse href="CollapseExample3">
+                            <Card
+                                cardTitle="Card Title III"
+                                cardText="lorem ıpsum dolor sit amet"
+                                updatedTime="6 min ago"
+                                image="https://picsum.photos/id/100/200/300"
+                            ></Card>
+                        </Collapse>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
